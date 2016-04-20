dca334104098a1f9f71637321def3f648d18a4c6
Jason Campbell <jrcampbell78@gmail.com>
2016-04-19 22:31:03 -0400

    wrote slice and split, passing all tests

--- 03.String.js
+++ 03.String.js
@@ -10,7 +10,39 @@
 // index (non-inclusive). The `Array.prototype` has a very similar 
 // method. Design your `slice` to work for both strings and arrays.
 var slice = function(stringOrArray, start, end) {
+	
+	if(end === undefined){
+		end = stringOrArray.length;
+	}
+	if (end > stringOrArray.length){
+		end = stringOrArray.length;
+	}
+	if (end < 0){
+		end = stringOrArray.length + end;
+	}
 
+	if(start === undefined){
+		start = 0;
+	}
+	if (start < 0){
+		start = stringOrArray.length + start;
+	}
+	
+	if(typeof stringOrArray === "string"){
+		var string = '';
+
+
+		for(var i = start; i < end; i++){
+			string += stringOrArray.charAt(i);
+		}
+		return string;
+	}
+
+	var arr = [];
+for(var j = start; j < end; j++){
+  push(arr, stringOrArray[j]);
+}
+return arr;
 };
 
 
@@ -18,8 +50,57 @@ var slice = function(stringOrArray, start, end) {
 // breaking it up using a delimiter string. The delimiter is not 
 // included in the final array.
 var split = function(string, delim) {
+  
+    var arr = [];
+    var splicer = '';
+    var nospaceArr = [];
+    var joiner = '';
+    var final = [];
+    var undefArr = [];
+       if (delim === undefined){  
+          undefArr[0] = string;
+          return undefArr;
+       };
+   
+    var splicer = ''
+     for(var u = 0; u < string.length; u++){
+         if(delim === ''){
+        for (var i = 0; i < string.length; i++){
+        push(nospaceArr, string[i])
+        }
+        return nospaceArr;
+         }
 
-};
+   
+     splicer = slice(string, u, u+delim.length);
+     if (delim === splicer){
+     push(final,u);
+     u+= delim.length-1
+     splicer = ''
+     } 
+     };
+     
+     if (final.length === 0){
+         return final
+     }
+     final[final.length] = string.length;
+     
+    for(var x = 0; x < final.length; x++){
+            for(var y = 0; y < string.length; y++){
+            if(y === final[x]){
+                y = y+ delim.length;
+                x = x+1;
+            }
+            tempString = slice(string, y,final[x]);
+            push(arr, tempString);
+            y = final[x] -1;
+            }
+         
+        }
+    
+      
+      return arr;
+}; 
 
 
 // `String.prototype.trim` is a handy little method for processing text.
